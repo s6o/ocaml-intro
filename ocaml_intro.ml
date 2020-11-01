c29184c02c4888e6c27c8578bfe31f0d7b2a1e69
Oliver Sõro <oliver.s6ro@gmail.com>
2020-11-01 08:30:07 +0200

    Conclusion of Core Concepts.

--- ocaml_intro.ml
+++ ocaml_intro.ml
@@ -1,31 +1,14 @@
-open Core_kernel
+open Base
+open Stdio
 
-type running_sum =
-  { mutable sum : float
-  ; mutable sum_sq : float
-  ; mutable samples : int
-  }
-
-let mean rsum = rsum.sum /. Float.of_int rsum.samples
-
-let stdev rsum =
-  Float.sqrt
-    ( (rsum.sum_sq /. Float.of_int rsum.samples)
-    -. ((rsum.sum /. Float.of_int rsum.samples) **. 2.) )
+let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None ->
+      accum
+  | Some x ->
+      read_and_accumulate (accum +. Float.of_string x)
 
 
-let create () = { sum = 0.; sum_sq = 0.; samples = 0 }
-
-let update rsum x =
-  rsum.samples <- rsum.samples + 1 ;
-  rsum.sum <- rsum.sum +. x ;
-  rsum.sum_sq <- rsum.sum_sq +. (x *. x)
-
-
-let rsum = create ()
-
-let () =
-  List.iter [ 1.; 3.; 2.; -7.; 4.; 5. ] ~f:(fun x -> update rsum x) ;
-  let result1 = mean rsum in
-  let result2 = stdev rsum in
-  Format.printf "Results %f %f" result1 result2
+(** Enter number on command line with enter and press Ctrl+D to calculate sum *)
+let () = printf "Total: %F\n" (read_and_accumulate 0.)
