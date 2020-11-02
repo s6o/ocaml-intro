897c16aea35ea69440a56f593a43bb18fe045562
Oliver Sõro <oliver.s6ro@gmail.com>
2020-11-03 00:21:33 +0200

    Add list based data table formatting example for console.

--- ocaml_intro.ml
+++ ocaml_intro.ml
@@ -1,14 +1,42 @@
 open Base
 open Stdio
 
-let rec read_and_accumulate accum =
-  let line = In_channel.input_line In_channel.stdin in
-  match line with
-  | None ->
-      accum
-  | Some x ->
-      read_and_accumulate (accum +. Float.of_string x)
+let max_widths header rows =
+  let lengths l = List.map ~f:String.length l in
+  List.fold rows ~init:(lengths header) ~f:(fun acc row ->
+      List.map2_exn ~f:Int.max acc (lengths row))
 
 
-(** Enter number on command line with enter and press Ctrl+D to calculate sum *)
-let () = printf "Total: %F\n" (read_and_accumulate 0.)
+let render_border widths =
+  let columns = List.map widths ~f:(fun w -> String.make (w + 2) '-') in
+  "|" ^ String.concat ~sep:"+" columns ^ "|"
+
+
+let col_pad s length = " " ^ s ^ String.make (length - String.length s + 1) ' '
+
+let render_row row widths =
+  let padded = List.map2_exn row widths ~f:col_pad in
+  "|" ^ String.concat ~sep:"|" padded ^ "|"
+
+
+let render_table header rows =
+  let widths = max_widths header rows in
+  String.concat
+    ~sep:"\n"
+    ( render_row header widths
+    :: render_border widths
+    :: List.map rows ~f:(fun row -> render_row row widths) )
+
+
+let data =
+  ( [ "language"; "architect"; "first release" ]
+  , [ [ "Lisp"; "John McCarthy"; "1958" ]
+    ; [ "C"; "Dennis Ritchie"; "1969" ]
+    ; [ "ML"; "Robin Milner"; "1973" ]
+    ; [ "OCaml"; "Xavier Leroy"; "1996" ]
+    ] )
+
+
+let () =
+  let header, rows = data in
+  render_table header rows |> printf "%s\n"
