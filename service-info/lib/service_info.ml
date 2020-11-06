db8a43fc55abb944604ac1697b66d62a286b45a5
Oliver Sõro <oliver.s6ro@gmail.com>
2020-11-07 00:02:29 +0200

    Add comment member to service_info record, update functions.

--- service-info/lib/service_info.ml
+++ service-info/lib/service_info.ml
@@ -15,18 +15,28 @@ type service_info =
   { service_name : string
   ; port : int
   ; protocol : string
+  ; comment : string option
   }
 
 let service_info_item line =
+  let header, comment =
+    match String.rsplit2 line ~on:'#' with
+    | None ->
+        (line, None)
+    | Some (h, c) ->
+        (h, Some c)
+  in
   let matches =
     let pattern = "([a-zA-Z]+)[ \t]+([0-9]+)/([a-zA-Z]+)" in
-    Re.exec (Re.Posix.compile_pat pattern) line
+    Re.exec (Re.Posix.compile_pat pattern) header
   in
   { service_name = Re.Group.get matches 1
   ; port = Int.of_string (Re.Group.get matches 2)
   ; protocol = Re.Group.get matches 3
+  ; comment
   }
 
 
-let service_info_to_string { service_name; port; protocol } =
-  sprintf "%s %i/%s" service_name port protocol
+let service_info_to_string { service_name; port; protocol; comment } =
+  let base = sprintf "%s %i/%s" service_name port protocol in
+  match comment with None -> base | Some c -> base ^ "\t#" ^ c
