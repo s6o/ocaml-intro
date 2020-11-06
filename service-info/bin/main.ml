001d6daea4ac14bc654c807f9220cdba1134621e
Oliver Sõro <oliver.s6ro@gmail.com>
2020-11-06 23:40:19 +0200

    Add polymorphic record type, record to string conversion to service-info.

--- service-info/bin/main.ml
+++ service-info/bin/main.ml
@@ -1,10 +1,17 @@
+open Core
 open Service_info
 
 let ssh = service_info_item "ssh 22/udp # SSH Remote Login Protocol"
 
+let services_test =
+  "rtmp              1/ddp     # Routing Table Maintenance Protocol\n\
+  \  tcpmux            1/udp     # TCP Port Service Multiplexer\n\
+  \  tcpmux            1/tcp     # TCP Port Service Multiplexer"
+
+
+let numbered_lines = services_test |> parse_lines ~f:service_info_item
+
 let () =
-  Core.printf
-    "Service: %s | port %d | proto %s\n"
-    ssh.service_name
-    ssh.port
-    ssh.protocol
+  numbered_lines
+  |> List.iter ~f:(fun r ->
+         printf "%i\t%s\n" r.number (service_info_to_string r.item))
--- service-info/lib/service_info.ml
+++ service-info/lib/service_info.ml
@@ -1,5 +1,16 @@
 open Core
 
+type 'a with_line_number =
+  { item : 'a
+  ; number : int
+  }
+
+let parse_lines file_contents ~f:parse =
+  let lines = file_contents |> String.split ~on:'\n' in
+  List.mapi lines ~f:(fun line_num line ->
+      { item = parse line; number = line_num + 1 })
+
+
 type service_info =
   { service_name : string
   ; port : int
@@ -15,3 +26,7 @@ let service_info_item line =
   ; port = Int.of_string (Re.Group.get matches 2)
   ; protocol = Re.Group.get matches 3
   }
+
+
+let service_info_to_string { service_name = sn; port = p; protocol = proto } =
+  sprintf "%s %i/%s" sn p proto
